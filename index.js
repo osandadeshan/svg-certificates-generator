2a43c6e312c387861a03f337ff550717871d5c1c
andunkannangara <60342777+andunkannangara@users.noreply.github.com>
2021-06-29 00:17:00 +0530

    Add read from s3 and upload to s3 with test file

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 const serverless = require('serverless-http');
 const express = require('express')
 const app = express();
+const s3 = require('aws-sdk/clients/s3');
+require('dotenv').config()
 
-const linkedinProfileUrl = 'https://run.mocky.io/v3/66de2cca-4f16-45ee-b3d5-efd28da73b49';
+app.post('/certificate', function (req, res) {
+    const data = JSON.parse(req.body);
+    const userParams = {
+        Certificate: {
+            template: data.template,
+            certificateName: data.certificateName,
+            recognition: data.recognition,
+            signature: data.signature,
+            studentName: data.studentName,
+            logo: data.logo,
+            mascotLogo: data.mascotLogo,
+            dateCreated: new Date().getTime()
+        }
+    };
+    const readSvgFile = () => {
+        return new Promise(function (resolve, reject) {
+            let s3Bkt = new s3({ accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET });
+            let params = { Bucket: process.env.AWS_S3_BUCKET, Key: 'svg.svg' };
+            s3Bkt.getObject(params, function (err, data) {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    const data = Buffer.from(data.Body).toString('utf8');
+                    resolve(data);
+                }
+            });
+        });
+    }
+    let message;
+    const uploadFile = () => {
+        return new Promise(function (resolve, reject) {
+            readSvgFile().then(data => {
+                data = data.replace("Name", userParams.Certificate.studentName)
+                data = data.replace("Signature", userParams.Certificate.signature)
+                const params = {
+                    Bucket: process.env.AWS_S3_BUCKET,
+                    Key: 'svg12.svg', // File name you want to save as in S3
+                    Body: data
+                };
+                let s3Bkt = new s3({ accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET });
+                s3Bkt.upload(params, function(err, data) {
+                    if (err) {
+                        reject(err.message)
+                    } else {
+                        message = `File uploaded successfully. ${data.Location}`
+                        resolve(message);
+                    }
+                });
+            })
+        })
+    };
 
-app.get('/', function (req, res) {
+    uploadFile().then(data => console.log(data)).catch(err => console.log(err))
+
     res.status(200).json({
-        message: "Hello Serverless!"
+        message: message
     });
 });
 
---
+++ tes.js
@@ -0,0 +1,74 @@
+const {SVGPathData, SVGPathDataTransformer, SVGPathDataEncoder, SVGPathDataParser} = require('svg-pathdata');
+var fs = require('fs');
+const s3 = require('aws-sdk/clients/s3');  //
+// let data2;
+// fs.readFile('https://certificates-andun.s3.amazonaws.com/svg.svg', 'utf8' , (err, data) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     // console.log(data)
+//     data2 = data.replace("Name", "Andun");
+//     data2 = data.replace("Signature", "AndunRanmal");
+//
+//
+//     dom.window.document.getElementsByTagName('text')[1].textContent = "Andun"
+//     console.log(dom.window.document.getElementsByTagName('text')[1].textContent)
+//
+//
+//     console.log(data);
+//     fs.writeFile('test.svg', data, (err) => {
+//         // throws an error, you could also catch it here
+//         if (err) throw err;
+//
+//         // success case, the file was saved
+//         console.log('Lyric saved!');
+//     });
+// })
+async function readFile() {
+    try {
+        let s3Data = await readSvgFile();
+        console.log(s3Data);
+    } catch (err) {
+        console.log('Error:', err);
+    }
+}
+
+const readSvgFile = () => {
+    return new Promise(function (resolve, reject) {
+        var s3Bkt = new s3({ accessKeyId: "AKIAZVNZ5GDWAM364PPF", secretAccessKey: "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" });
+        // var s3Bkt = new s3();
+        var params = { Bucket: "certificates-andun", Key: 'svg.svg' };
+        s3Bkt.getObject(params, function (err, data) {
+            if (err) {
+                reject(err.message);
+            } else {
+                var data = Buffer.from(data.Body).toString('utf8');
+                resolve(data);
+            }
+        });
+    });
+}
+
+const uploadFile = (fileName) => {
+    // Read content from the file
+    let fileContent;
+    readSvgFile().then(data => {
+        const params = {
+            Bucket: "certificates-andun",
+            Key: 'svg2.svg', // File name you want to save as in S3
+            Body: data
+        };
+        let s3Bkt = new s3({ accessKeyId: "AKIAZVNZ5GDWAM364PPF", secretAccessKey: "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" });
+        s3Bkt.upload(params, function(err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(`File uploaded successfully. ${data.Location}`);
+        });
+    })
+};
+
+uploadFile()
+
+
